2875c2ca203e11ba11e35b5b80a306cb9db4d6f6
MarkUA <mark.d.nahorniak@gmail.com>
2024-11-09 19:10:30 +0200

    fix

---
+++ indes.js
@@ -0,0 +1,17 @@
+// TASK 1
+// const friendList = ["mango", "poly", "kiwi", "ayax"]
+// const friends = friendList.join(", ")
+// console.log(friends)
+
+// TASK 2
+const cardList = ["card-1", "card-2", "card-3", "card-4", "card-5"]
+const cards = cardList.splice("")
+console.log(cards)
+
+cards.pop("")
+cards.pop("")
+cards.pop("")
+cards.push("card-4")
+cards.push("card-5")
+cards.push("card-6")
+console.log(cards)
